bc5bb5d46f52b4868613760eced4eaaaf14c0ab5
CDelc <77530228+CDelc@users.noreply.github.com>
2022-03-03 18:26:27 -0500

    Argument Handling, Copying input files and directories

--- the-obfuscatorinator/src/main/java/com/theobfuscatorinator/App.java
+++ the-obfuscatorinator/src/main/java/com/theobfuscatorinator/App.java
@@ -1,18 +1,41 @@
 package com.theobfuscatorinator;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
+        /* Try/Catch Block for custom error message.*/
         try{
             if(args.length == 0) throw new IllegalArgumentException("Not enough arguments");
+            String copyPath = "output";
+            Vector<String> inputFiles = new Vector<String>();
+
+            //Argument Handling
+            for(int i = 0; i < args.length; i++){
+                if(args[i].equals("--target")){
+                    i++;
+                    if(i >= args.length) throw new IllegalArgumentException("Target Missing");
+                    copyPath = args[i];
+                }
+                else{
+                    if(!(new File(args[i])).exists()) throw new IllegalArgumentException("Source File " + args[i] + " does not exist.");
+                    inputFiles.add(args[i]);
+                }
+            }
+            if(inputFiles.size() == 0) throw new IllegalArgumentException("No Source Files Provided");
+            File targetDirectory = FileManager.copyAndStoreFiles(inputFiles, copyPath);
+
         }
         catch(Exception e){
-            System.err.print("Exception thrown: " + e.getMessage() + "\n" + e.toString() + "\n");
+            System.err.println("Exception thrown: " + e.getMessage());
+            e.printStackTrace();
             System.exit(1);
         }
     }
--- the-obfuscatorinator/src/main/java/com/theobfuscatorinator/FileManager.java
+++ the-obfuscatorinator/src/main/java/com/theobfuscatorinator/FileManager.java
@@ -1,7 +1,47 @@
 package com.theobfuscatorinator;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Vector;
+
 public class FileManager {
-    
-    public FileManager(){}
+
+
+    private static void copyFile(File source, String destinationDirectory) throws IOException {
+        File destination = new File(destinationDirectory + "/" +  source.getName());
+        if (destination.exists()) destination.delete();
+        //destination.createNewFile();
+        System.out.println("Copying File " + source.toString() + " to destination " + destination.toString());
+        Files.copy(source.toPath(), destination.toPath());
+    }
+
+    private static void copyDirectory(File source, String destinationDirectory) throws IOException {
+        if (source.isDirectory()) {
+            File destination = new File(destinationDirectory + "/" +  source.getName());
+            if (destination.exists()) destination.delete();
+            destination.mkdir();
+            for (File child : source.listFiles()) copyDirectory(child, destination.toString());
+        } else copyFile(source, destinationDirectory);
+    }
+
+    public static File copyAndStoreFiles(Vector<String> files, String targetDir) throws IOException {
+        File target = new File(targetDir);
+        if (!target.exists()) {
+            target.mkdir();
+        }
+        if (!target.isDirectory()) throw new IllegalArgumentException("Target File is not a directory ");
+
+        for (String path : files) {
+            File tmpFile = new File(path);
+            if (tmpFile.isDirectory()) {
+                copyDirectory(tmpFile, targetDir);
+            } else {
+                copyFile(tmpFile, targetDir);
+            }
+
+        }
+        return target;
+    }
 
 }
