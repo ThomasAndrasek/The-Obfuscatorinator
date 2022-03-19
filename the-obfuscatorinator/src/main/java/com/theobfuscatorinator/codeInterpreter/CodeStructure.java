f943a8fe96a0e8a72ec05147d4f2556890a2ca98
CDelc <77530228+CDelc@users.noreply.github.com>
2022-03-19 14:36:56 -0400

    Added some starting documentation to CodeStructure

--- the-obfuscatorinator/src/main/java/com/theobfuscatorinator/codeInterpreter/CodeStructure.java
+++ the-obfuscatorinator/src/main/java/com/theobfuscatorinator/codeInterpreter/CodeStructure.java
@@ -5,13 +5,26 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.regex.Pattern;
 
+/**
+ * This class will take a java code file and will find important characteristics in that file which can be
+ * accessed by a user of this class.
+ *
+ * @author Carter Del Ciello
+ */
+
 public class CodeStructure {
 
     File codeFile;
     String fileName;
-    public String originalCode;
+    String originalCode;
     String unCommentedCode;
 
+    /**
+     * Constructor will build all of the structure for you
+     * @param input File to be searched for code elements
+     * @throws IllegalArgumentException thrown if a directory is given instead of a regular file
+     * @throws IOException thrown from Files.readAllBytes
+     */
     public CodeStructure(File input) throws IllegalArgumentException, IOException {
         if(!input.isDirectory()){
             codeFile = input;
@@ -22,6 +35,11 @@ public class CodeStructure {
         else throw new IllegalArgumentException("Cannot make a code structure out of a directory.");
     }
 
+    /**
+     * Takes some java code as a string and removes the comments from it. Does not modify the original code.
+     * @param code Code to have the comments removed from
+     * @return Copy of code without any comments
+     */
     private String removeComments(String code){
         String copy = code.trim();
         String output = "";
