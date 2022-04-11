08efe23364b7ccb250c7fe00237b1816113bb59a
nathanWhitney <68211239+nathanWhitney@users.noreply.github.com>
2022-04-11 11:42:11 -0400

    Started prepping testing code

--- the-obfuscatorinator/src/test/java/com/theobfuscatorinator/CodeStructureTest.java
+++ the-obfuscatorinator/src/test/java/com/theobfuscatorinator/CodeStructureTest.java
@@ -3,6 +3,10 @@ package com.theobfuscatorinator;
 import com.theobfuscatorinator.codeInterpreter.CodeStructure;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.tools. *;
 
 import org.junit.Test;
 
