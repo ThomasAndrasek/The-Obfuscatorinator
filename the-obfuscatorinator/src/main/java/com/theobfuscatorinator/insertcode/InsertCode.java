e670ce882e15d3e89d255b59dfe07c81002597ff
mindyyip <mindyyip81@gmail.com>
2022-04-12 02:04:31 -0400

    added insertcode initial code

---
+++ the-obfuscatorinator/src/main/java/com/theobfuscatorinator/insertcode/InsertCode.java
@@ -0,0 +1,41 @@
+package com.theobfuscatorinator.insertcode;
+
+import java.util.ArrayList;
+
+import com.theobfuscatorinator.codeInterpreter.CodeStructure;
+//import com.theobfuscatorinator.stringencryption.StringEncryption.Pair;
+
+public class InsertCode{
+	
+	static class Pair<K,V>{
+		public K first;
+		public V second;
+		
+		public Pair(K fst, V snd) {
+			first = fst;
+			second = snd;
+		}
+	}
+    private static ArrayList<Pair<String, Integer>> findStrings(CodeStructure codeStructure) {
+        ArrayList<Pair<String, Integer>> strings = new ArrayList<Pair<String, Integer>>();
+        String code = codeStructure.getUnCommentedCode();
+
+        int i = 0;
+        while (i < code.length()) {
+            if (code.charAt(i) == '"') {
+                int j = i + 1;
+                while (j < code.length() && (code.charAt(j) != '"' || code.charAt(j) == '\\')) {
+                    j++;
+                }
+                if (j < code.length()) {
+                    strings.add(new Pair<String, Integer>(code.substring(i + 1, j), i));
+                    i = j + 1;
+                }
+            }
+            i++;
+        }
+        System.out.println(strings);
+        return strings;
+    }
+
+}
---
+++ the-obfuscatorinator/src/test/java/com/theobfuscatorinator/InsertCodeTest.java
@@ -0,0 +1,19 @@
+package com.theobfuscatorinator;
+
+import com.theobfuscatorinator.insertcode.InsertCode;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class InsertCodeTest {
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+
+
+}
