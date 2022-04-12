780738f417a18ddd003f003be8767e2608a850b6
nathanWhitney <68211239+nathanWhitney@users.noreply.github.com>
2022-04-12 13:47:00 -0400

    Began building then made obsolete the CodeStructureTest file

--- the-obfuscatorinator/src/test/java/com/theobfuscatorinator/CodeStructureTest.java
+++ the-obfuscatorinator/src/test/java/com/theobfuscatorinator/CodeStructureTest.java
@@ -11,8 +11,12 @@ import javax.tools. *;
 import org.junit.Test;
 
 
+//obsolete shellscript work will handle changes
 public class CodeStructureTest {
-    
+	
+	public static void main(String args[]) throws IOException {
+		
+	}
     @Test
     public void testRemoveComments() {
         //assertTrue();
@@ -36,6 +40,22 @@ public class CodeStructureTest {
     @Test
     public void testGetCommaSeparatedValues() {
         //assertTrue();
+    }
+    
+    private static void checker() throws IOException {
+    	//Check stack overflow for more information
+    	//https://stackoverflow.com/questions/8363904/how-to-compile-java-file-from-within-java-program
+    	//still working on understanding this
+    	JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+		StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
+		Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromStrings(Arrays.asList("testing1.java"));
+		JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, diagnostics, null,
+		        null, compilationUnits);
+		boolean success = task.call();
+		fileManager.close();
+		System.out.println("Success: " + success);
+    	
     }
     
 
