ef9a96b91f25a94bbcdfbbe062bc7227dec98ee1
Mindy Yip <mindyyip81@gmail.com>
2022-04-16 00:59:21 -0400

    remove unused code from InsertCode

--- the-obfuscatorinator/src/main/java/com/theobfuscatorinator/insertcode/InsertCode.java
+++ the-obfuscatorinator/src/main/java/com/theobfuscatorinator/insertcode/InsertCode.java
@@ -16,38 +16,6 @@ public class InsertCode {
     private String fileName;
     private String originalCode;
 
-	static class Pair<K,V>{
-		public K first;
-		public V second;
-		
-		public Pair(K fst, V snd) {
-			first = fst;
-			second = snd;
-		}
-	}
-
-    private static ArrayList<Pair<String, Integer>> findStrings(CodeStructure codeStructure) {
-        ArrayList<Pair<String, Integer>> strings = new ArrayList<Pair<String, Integer>>();
-        String code = codeStructure.getUnCommentedCode();
-
-        int i = 0;
-        while (i < code.length()) {
-            if (code.charAt(i) == '"') {
-                int j = i + 1;
-                while (j < code.length() && (code.charAt(j) != '"' || code.charAt(j) == '\\')) {
-                    j++;
-                }
-                if (j < code.length()) {
-                    strings.add(new Pair<String, Integer>(code.substring(i + 1, j), i));
-                    i = j + 1;
-                }
-            }
-            i++;
-        }
-        System.out.println(strings);
-        return strings;
-    }
-    
     public static String generateDummyClass() {
     	String className = "DummyCode";
     	String startClassCode = "public static class " + className + " {";
