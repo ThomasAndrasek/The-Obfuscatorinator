e5bde2c9776d6f1fcfc70de63c3bcd369ec5a426
nathanWhitney <68211239+nathanWhitney@users.noreply.github.com>
2022-04-22 17:36:04 -0400

    Created CodeStructureTest

---
+++ the-obfuscatorinator/src/test/java/com/theobfuscatorinator/CodeStructureTest.java
@@ -0,0 +1,14 @@
+package com.theobfuscatorinator;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CodeStructureTest 
+{
+    
+}
