5117f8a99aef54bbb076b9e761ae46f2469d3247
Thomas Andrasek <contactThomasAndrasek@gmail.com>
2022-04-24 18:11:24 -0400

    Thomastesting (#26)
    * Fixed implements identification.
    
    Originally all classes, no matter what, were being identified as classes
    that did not implement an interface. That problem has been resolved.
    
    There was also an issue that which flipped what should happen when a
    class is identified as one that implements an interface. It has been
    resolved so that classes that do not implement an interface now have
    their methods identified and renamed while classes that do implement an
    interface do not.
    
    * Implement variable renaming.
    
    * Added command for ignoring renaming variables.

--- the-obfuscatorinator/src/main/java/com/theobfuscatorinator/App.java
+++ the-obfuscatorinator/src/main/java/com/theobfuscatorinator/App.java
@@ -26,7 +26,7 @@ public class App
 
             //Argument Handling
             boolean renameClasses = true, renameMethods = true, insertCode = true,
-                    addDecryption = true, unicode = true;
+                    addDecryption = true, unicode = true, renameVariables = true;
             int percentUnicode = 5;
             for(int i = 0; i < args.length; i++){
                 if(args[i].equals("--target")){
@@ -37,6 +37,7 @@ public class App
                 else if(args[i].equalsIgnoreCase("--nomethodrenames")) renameMethods = false;
                 else if(args[i].equalsIgnoreCase("--nofakecode")) insertCode = false;
                 else if(args[i].equalsIgnoreCase("--nounicode")) unicode = false;
+                else if(args[i].equalsIgnoreCase("--novariablerenames")) renameVariables = false;
                 else if(args[i].equalsIgnoreCase("--unicodeFreq")){
                     i++;
                     if(i >= args.length)
@@ -72,6 +73,12 @@ public class App
                 System.out.println("Renaming Methods...");
                 Renamer.renameMethods(codeStructures);
             }
+
+            if (renameVariables) {
+                System.out.println("Renaming Variables...");
+                Renamer.renameVariables(codeStructures);
+            }
+
             if(insertCode){
                 System.out.println("Inserting Dummy Code...");
                 InsertCode.insertCode(codeStructures);
