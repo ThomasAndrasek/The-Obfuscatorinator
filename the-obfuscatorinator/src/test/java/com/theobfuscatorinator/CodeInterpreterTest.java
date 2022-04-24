62a8767244dc6dee86d4c73657da15d33ad4476b
nathanWhitney <68211239+nathanWhitney@users.noreply.github.com>
2022-04-23 21:35:38 -0400

    Started writing up the CodeInterpreterTest

--- the-obfuscatorinator/src/test/java/com/theobfuscatorinator/CodeInterpreterTest.java
+++ the-obfuscatorinator/src/test/java/com/theobfuscatorinator/CodeInterpreterTest.java
@@ -5,15 +5,66 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
 
 public class CodeInterpreterTest {
 
 
     @Test
     public void testCodeStructure() {
+        File f = new File("test.java");
+        if(f.exists()){
+            CodeStructure t = null;
+            try{
+                t = new CodeStructure(f);
+            }catch(Exception e){
+                e.printStackTrace();
+                //assertTrue("IOException Thrown in constructor", false);
+            }
+        }
+    }
+    
+    @Test
+    public void testGetOriginalCode() throws IllegalArgumentException, IOException {
+        File f = new File("test.java");
+        CodeStructure t = new CodeStructure(f);
+        String oc = new String(Files.readAllBytes(f.toPath()));
+        String hold = t.getOriginalCode();
+        assertEquals(oc, hold);
+    }
+    
+    @Test
+    public void testRemoveComments() throws IllegalArgumentException, IOException {
+        File f = new File("test.java");
+        CodeStructure t = new CodeStructure(f);
+        //oc equals test without comments
+        String oc = "";
+        String hold = t.getUnCommentedCode();
+        assertEquals(oc, hold);
+    }
+    
+    
+    //not finished
+    @Test
+    public void testRemoveStrings() throws IllegalArgumentException, IOException {
+        File f = new File("test.java");
+        CodeStructure t = new CodeStructure(f);
+        //oc equals test without comments
+        String oc = "";
+        String hold = t.getUnCommentedCode();
+        assertEquals(oc, hold);
+    }
+    
+    
+    
+    //update
+    @Test
+    public void testStructure() {
         File f = new File("test.java");
         if(f.exists()){
             CodeStructure t = null;
