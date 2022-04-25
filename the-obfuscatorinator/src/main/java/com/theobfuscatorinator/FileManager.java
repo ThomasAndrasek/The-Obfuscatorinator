d06c6530738e1335a8b977dc65ad38cb64ddc3a6
CDelc <77530228+CDelc@users.noreply.github.com>
2022-04-25 16:06:54 -0400

    Dev c delc (#29)
    * Now deletes output directory if it exists before creating it
    
    * Fixed certain StringIndexOutOfBoundsException

--- the-obfuscatorinator/src/main/java/com/theobfuscatorinator/FileManager.java
+++ the-obfuscatorinator/src/main/java/com/theobfuscatorinator/FileManager.java
@@ -85,9 +85,24 @@ public class FileManager {
     public static File copyAndStoreFiles(Vector<String> files, String targetDir)
      throws IOException {
         File target = new File(targetDir);
-        if (!target.exists()) {
-            target.mkdir();
+        if (target.exists()) {
+            Path directory = target.toPath();
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+            target.delete();
         }
+        target.mkdir();
         if (!target.isDirectory())
              throw new IllegalArgumentException("Target File is not a directory ");
 
