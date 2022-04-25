0f6eff16bb160d410054f62675acb8b3dde4f39c
CDelc <77530228+CDelc@users.noreply.github.com>
2022-04-24 21:08:23 -0400

    Added documentation for Whitespace Handler

--- the-obfuscatorinator/src/main/java/com/theobfuscatorinator/codeInterpreter/WhitespaceHandler.java
+++ the-obfuscatorinator/src/main/java/com/theobfuscatorinator/codeInterpreter/WhitespaceHandler.java
@@ -2,14 +2,33 @@ package com.theobfuscatorinator.codeInterpreter;
 
 import java.util.*;
 
+/**
+ * This class contains a set of static methods designed to obfuscate a
+ * set of codestructures further with the use of white space removal and newline insertion
+ *
+ * @author Carter Del Ciello
+ */
+
 public class WhitespaceHandler {
 
+    /**
+     * Removes all the newlines in the project and then re-inserts a predetermined
+     * number of newlines back into each file at random locations.
+     * The locations of the newlines are selected so that the file
+     * still compiles and runs correctly.
+     * @param codeStructures List of CodeStructure objects that make up the project
+     * @param numNewlines The number of new newlines to place in each file
+     */
     public static void newlineClearAndInsert(ArrayList<CodeStructure> codeStructures,
                                              int numNewlines){
         clearNewlines(codeStructures);
         insertNewlines(codeStructures, numNewlines);
     }
 
+    /**
+     * Removes all the newlines in a project
+     * @param codeStructures List of CodeStructure objects that make up the project
+     */
     private static void clearNewlines(ArrayList<CodeStructure> codeStructures){
         for(CodeStructure struct : codeStructures){
             String code = struct.getUnCommentedCode();
@@ -21,6 +40,12 @@ public class WhitespaceHandler {
         }
     }
 
+    /**
+     * Inserts newlines into a project at a specified, random number of locations.
+     * File will still compile and run the same after being affected by this method.
+     * @param codeStructures List of CodeStructure objects that make up the project
+     * @param count The number of new newlines to place in each file
+     */
     private static void insertNewlines(ArrayList<CodeStructure> codeStructures, int count){
         if(count < 0) count *= -1;
         count--;
@@ -67,6 +92,10 @@ public class WhitespaceHandler {
         }
     }
 
+    /**
+     * Remove all spaces from project source code that are not necessary for program functionality.
+     * @param codeStructures List of CodeStructure objects that make up the project
+     */
     public static void removeSpaces(ArrayList<CodeStructure> codeStructures){
         char[] separators = {'=', '+', '-', '*', '/', '%', '!', '|', '&',
                 '(', ')', '{', '}', '[', ']', ';','\n', '\r', ' ', '\t'};
@@ -88,15 +117,31 @@ public class WhitespaceHandler {
         }
     }
 
-
+    /**
+     * Insert a characters into a string
+     * @param s String to insert into
+     * @param c Characters to insert
+     * @param index Location in string to insert
+     * @return The string with the added character
+     */
     private static String insertChar(String s, char c, int index){
+        if(index >= s.length() || index < 0)
+            throw new StringIndexOutOfBoundsException("Index out of bounds for string");
         String tmp = s.substring(0, index);
         tmp += c;
         tmp += s.substring(index);
         return tmp;
     }
 
+    /**
+     *
+     * @param s String to remove from
+     * @param index Index of the character to remove
+     * @return The string without the character
+     */
     private static String removeChar(String s, int index){
+        if(index >= s.length() || index < 0)
+            throw new StringIndexOutOfBoundsException("Index out of bounds for string");
         return s.substring(0, index).concat(s.substring(index + 1));
     }
 }
