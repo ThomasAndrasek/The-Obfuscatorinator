d9c246dfe200d4b4442def5c2db9c70c683189f1
thomasandrasek <contactthomasandrasek@gmail.com>
2022-12-09 23:02:40 -0500

    Added comments and equals method to import struct

--- the-obfuscatorinator/src/main/java/com/theobfuscatorinator/codeInterpreter/ImportStructure.java
+++ the-obfuscatorinator/src/main/java/com/theobfuscatorinator/codeInterpreter/ImportStructure.java
@@ -4,22 +4,40 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Represents an import statement in a Java file.
+ * 
+ * @author Thomas Andrasek
+ */
 public class ImportStructure {
     private boolean isStatic;
     private String[] packagePath;
     private boolean hasStar;
 
+    /**
+     * Constructs a structure to represent a Java import statement.
+     * 
+     * @param isStatic Whether the import is static.
+     * @param packagePath The path of the import statement.
+     */
     public ImportStructure(boolean isStatic, String packagePath) {
         this.isStatic = isStatic;
         this.hasStar = false;
 
         this.packagePath = packagePath.split("\\.");
 
+        // Check if import utilizes the '*'
         if (this.packagePath[this.packagePath.length-1].equals("*")) {
             this.hasStar = true;
         }
     }
 
+    /**
+     * Identifies all imports in a Java file.
+     * 
+     * @param codeStructure The code structure that represents a Java file.
+     * @return All of the identified import statements as structures.
+     */
     public static ArrayList<ImportStructure> identifyImports(CodeStructure codeStructure) {
         ArrayList<ImportStructure> importStructures = new ArrayList<>();
 
@@ -30,6 +48,7 @@ public class ImportStructure {
             boolean isStatic = false;
             String packagePath = importMatcher.group(2);;
 
+            // Check if the import is static.
             if (importMatcher.group(1) != null) {
                 isStatic = true;
             }
@@ -40,10 +59,21 @@ public class ImportStructure {
         return importStructures;
     }
 
+
+    /**
+     * Checks if the import is static. 
+     * 
+     * @return If the import is static. 
+     */
     public boolean isStatic() {
         return this.isStatic;
     }
 
+    /**
+     * Get the package path of the import statement.
+     * 
+     * @return The package path of the import statement.
+     */
     public String[] getPackagePath() {
         String[] path = new String[this.packagePath.length];
         for (int i = 0; i < path.length; i++) {
@@ -53,6 +83,11 @@ public class ImportStructure {
         return path;
     }
 
+    /**
+     * Checks if the import statement utilizes the '*'
+     * 
+     * @return If the import statement utilizes the '*'
+     */
     public boolean hasStar() {
         return this.hasStar;
     }
@@ -72,5 +107,40 @@ public class ImportStructure {
         ret += this.packagePath[this.packagePath.length-1];
 
         return ret;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (!(o instanceof ImportStructure)) {
+            return false;
+        }
+
+        ImportStructure other = (ImportStructure) o;
+
+        if (this.isStatic != other.isStatic) {
+            return false;
+        }
+
+        if (this.hasStar != other.hasStar) {
+            return false;
+        }
+
+        if (this.packagePath.length != other.packagePath.length) {
+            return false;
+        }
+
+        // Check if the package paths are equal.
+        for (int i = 0; i < this.packagePath.length; i++) {
+            // If one part of the path is not equal then they are not equal.
+            if (!(this.packagePath[i].equals(other.packagePath[i]))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
